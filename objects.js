ad96c103df8b5a4780afe670daadf866eef26ff1
Juliana Kang <julianajlk@gmail.com>
2018-05-11 12:35:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete 'Phil Ochs'['Slowdive'];
+  delete playlist['Slowdive'];
   return playlist;
 }
